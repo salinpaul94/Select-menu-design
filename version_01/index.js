3a74362aae388e02bd85e85595b6c97007ac73f1
salinpaul valooran <salinpv@hotmail.com>
2023-11-09 01:55:45 +0530

    function for showing select list on clicking dropdown arrow

--- version_01/index.js
+++ version_01/index.js
@@ -2,9 +2,10 @@ let selectField = document.getElementById("selectField");
 let selectText = document.getElementById("selectText");
 let options = document.getElementsByClassName("options");
 
+selectField.onclick = function () {};
+
 for (let option of options) {
   option.onclick = function () {
     selectText.innerHTML = this.textContent;
   };
 }
-
