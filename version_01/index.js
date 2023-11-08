1f47837f0352ddd4460495d164a02629de348359
salinpaul valooran <salinpv@hotmail.com>
2023-11-09 02:03:29 +0530

    toggle function to rotate icon for onclick event

--- version_01/index.js
+++ version_01/index.js
@@ -2,15 +2,17 @@ let selectField = document.getElementById("selectField");
 let selectText = document.getElementById("selectText");
 let options = document.getElementsByClassName("options");
 let list = document.getElementById("list");
-let rotateIcon = document.getElementById("rotateIcon");
+let arrowIcon = document.getElementById("arrowIcon");
 
 selectField.onclick = function () {
   list.classList.toggle("hide");
+  arrowIcon.classList.toggle("rotate");
 };
 
 for (let option of options) {
   option.onclick = function () {
     selectText.innerHTML = this.textContent;
     list.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate");
   };
 }
