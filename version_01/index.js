fa0de36e332746159efd3e0d0a93e728fa52a97c
salinpaul valooran <salinpv@hotmail.com>
2023-11-09 01:57:38 +0530

    adding toggle function to selectField on click to display and hide select list

--- version_01/index.js
+++ version_01/index.js
@@ -3,7 +3,9 @@ let selectText = document.getElementById("selectText");
 let options = document.getElementsByClassName("options");
 let list = document.getElementById("list");
 
-selectField.onclick = function () {};
+selectField.onclick = function () {
+  list.classList.toggle("hide");
+};
 
 for (let option of options) {
   option.onclick = function () {
